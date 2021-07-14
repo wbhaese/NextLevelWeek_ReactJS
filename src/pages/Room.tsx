05c4b8ce572749110c4c4bd438752cd3dfab75b5
unknown <wbhaese@gmail.com>
2021-07-14 17:17:37 -0300

    save progress till 1:01:00 of third class

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,13 +1,25 @@
+import { useParams } from 'react-router-dom';
+
 import logImg from '../assets/images/logo.svg';
+
 import { Button } from '../components/Button';
+import { RoomCode } from '../components/RoomCode';
+
+import '../styles/room.scss';
+
+type RoomParams = {
+    id: string
+}
 
 export function Room() {
+    const params = useParams<RoomParams>();
+
     return (
         <div id="page-room">
             <header>
                 <div className="content">
                     <img src={logImg} alt="Letmeask" />
-                    <div>Código</div>
+                    <RoomCode code={params.id} />
 
                 </div>
             </header>
